421062181eef8d028a486541be4aea38f9e3c27b
Nik <mail-an-nik0405@web.de>
2021-08-01 13:18:18 +0200

    cleaned up

--- kernel.cu
+++ kernel.cu
@@ -2,9 +2,6 @@
 
 #define TX 32
 #define TY 32
-#define XSTR(x) STR(x)
-//#define STR(x) #x
-//#pragma message("XSTR=" XSTR(__CUDA_ARCH__))
 
 __device__
 unsigned char clip(int n) { return n > 255 ? 255 : (n < 0 ? 0 : n); }
@@ -12,12 +9,12 @@ unsigned char clip(int n) { return n > 255 ? 255 : (n < 0 ? 0 : n); }
 template<typename T>
 __global__
 void distanceKernel(uchar4 *d_out, int w, int h, int2 pos) {
-    const int c = blockIdx.x * blockDim.x + threadIdx.x;
-    const int r = blockIdx.y * blockDim.y + threadIdx.y;
-    if ((c >= w) || (r >= h)) return; // Check if within image bounds
-    const int i = c + r * w; // 1D indexing
-    const int dist = sqrtf((c - pos.x) * (c - pos.x) +
-                           (r - pos.y) * (r - pos.y));
+    const int x = blockIdx.x * blockDim.x + threadIdx.x;
+    const int y = blockIdx.y * blockDim.y + threadIdx.y;
+    if ((x >= w) || (y >= h)) return; // Check if within image bounds
+    const int i = x + y * w; // 1D indexing
+    const int dist = sqrtf((x - pos.x) * (x - pos.x) +
+                           (y - pos.y) * (y - pos.y));
     const unsigned char intensity = clip(255 - dist);
     d_out[i].x = intensity;
     d_out[i].y = intensity;
@@ -26,7 +23,9 @@ void distanceKernel(uchar4 *d_out, int w, int h, int2 pos) {
 }
 
 void kernelLauncher(uchar4 *d_out, int w, int h, int2 pos) {
-    const dim3 blockSize(TX, TY);
-    const dim3 gridSize = dim3((w + TX - 1) / TX, (h + TY - 1) / TY);
+    // 3d version
+    const dim3 blockSize(TX, TY, 1);
+    const dim3 gridSize = dim3((w + TX - 1) / TX, (h + TY - 1) / TY, 1); // + TX - 1 for w size that is not divisible by TX
+
     distanceKernel<float><<<gridSize, blockSize>>>(d_out, w, h, pos);
 }
