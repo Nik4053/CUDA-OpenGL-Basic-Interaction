ceab6e42074188a79a6898a134aa80927c09af7c
Nik <mail-an-nik0405@web.de>
2021-08-01 11:56:16 +0200

    auto-ident

--- interactions.h
+++ interactions.h
@@ -4,44 +4,41 @@
 #define H 1600
 #define DELTA 5 // pixel increment for arrow keys
 #define TITLE_STRING "flashlight: distance image display app"
-int2 loc = {W/2, H/2};
+int2 loc = {W / 2, H / 2};
 bool dragMode = false; // mouse tracking mode
 
 void keyboard(unsigned char key, int x, int y) {
-  if (key == 'a') dragMode = !dragMode; // toggle tracking mode
-  if (key == 27)  exit(0);
-  glutPostRedisplay();
+    if (key == 'a') dragMode = !dragMode; // toggle tracking mode
+    if (key == 27) exit(0);
+    glutPostRedisplay();
 }
 
 void mouseMove(int x, int y) {
-  if (dragMode) return;
-  loc.x = W * x/ glutGet(GLUT_WINDOW_WIDTH);
-  loc.y = H * y/ glutGet(GLUT_WINDOW_HEIGHT);
-  glutPostRedisplay();
+    if (dragMode) return;
+    loc.x = W * x / glutGet(GLUT_WINDOW_WIDTH);
+    loc.y = H * y / glutGet(GLUT_WINDOW_HEIGHT);
+    glutPostRedisplay();
 }
 
 void mouseDrag(int x, int y) {
-  if (!dragMode) return;
-  loc.x = W * x / glutGet(GLUT_WINDOW_WIDTH);
-  loc.y = H * y / glutGet(GLUT_WINDOW_HEIGHT);
-  glutPostRedisplay();
+    if (!dragMode) return;
+    loc.x = W * x / glutGet(GLUT_WINDOW_WIDTH);
+    loc.y = H * y / glutGet(GLUT_WINDOW_HEIGHT);
+    glutPostRedisplay();
 }
+
 bool leftMouseButtonDown = 0;
 bool rightMouseButtonDown = 0;
 bool middleMouseButtonDown = 0;
-void mouse(int button, int state, int x, int y)
-{
+
+void mouse(int button, int state, int x, int y) {
     // Save the left button state
-    if (button == GLUT_LEFT_BUTTON)
-    {
+    if (button == GLUT_LEFT_BUTTON) {
         leftMouseButtonDown = (state == GLUT_DOWN);
-    }
-    else if (button == GLUT_RIGHT_BUTTON)
-    {
+    } else if (button == GLUT_RIGHT_BUTTON) {
         // right MouseButton
         rightMouseButtonDown = (state == GLUT_DOWN);
-    } else if (button == GLUT_MIDDLE_BUTTON)
-    {
+    } else if (button == GLUT_MIDDLE_BUTTON) {
         // middle MouseButton
         middleMouseButtonDown = (state == GLUT_DOWN);
     }
@@ -51,19 +48,16 @@ void mouse(int button, int state, int x, int y)
 //    mousePos.x = x;
 //    mousePos.y = y;
 }
-void mouseWheel(int button, int dir, int x, int y)
-{
+
+void mouseWheel(int button, int dir, int x, int y) {
 //    if(button&1<<0) printf("Left Mouse Button\n");
 //    if(button&1<<1) printf("right Mouse Button\n");
 //    //if(button&1<<2) printf("??1 Mouse Button\n");
 //    //if(button&1<<3) printf("??2 Mouse Button\n");
 //    if(button&1<<4) printf("Middle Mouse Button\n");
-    if (dir > 0)
-    {
+    if (dir > 0) {
         // Zoom in
-    }
-    else
-    {
+    } else {
         // Zoom out
     }
 
@@ -71,18 +65,18 @@ void mouseWheel(int button, int dir, int x, int y)
 }
 
 void handleSpecialKeypress(int key, int x, int y) {
-  if (key == GLUT_KEY_LEFT)  loc.x -= DELTA;
-  if (key == GLUT_KEY_RIGHT) loc.x += DELTA;
-  if (key == GLUT_KEY_UP)    loc.y -= DELTA;
-  if (key == GLUT_KEY_DOWN)  loc.y += DELTA;
-  glutPostRedisplay();
+    if (key == GLUT_KEY_LEFT) loc.x -= DELTA;
+    if (key == GLUT_KEY_RIGHT) loc.x += DELTA;
+    if (key == GLUT_KEY_UP) loc.y -= DELTA;
+    if (key == GLUT_KEY_DOWN) loc.y += DELTA;
+    glutPostRedisplay();
 }
 
 void printInstructions() {
-  printf("flashlight interactions\n");
-  printf("a: toggle mouse tracking mode\n");
-  printf("arrow keys: move ref location\n");
-  printf("esc: close graphics window\n");
+    printf("flashlight interactions\n");
+    printf("a: toggle mouse tracking mode\n");
+    printf("arrow keys: move ref location\n");
+    printf("esc: close graphics window\n");
 }
 
 #endif
--- main.cpp
+++ main.cpp
@@ -1,27 +1,32 @@
 #include "kernel.h"
 #include <stdio.h>
 #include <stdlib.h>
-#ifdef _WIN32
-    // save diagnostic state
-    #pragma warning( push, 3 )
 
-    #define WINDOWS_LEAN_AND_MEAN
-    #define NOMINMAX
-    #define WIN32_LEAN_AND_MEAN
-    #include <Windows.h>
+#ifdef _WIN32
+// save diagnostic state
+#pragma warning( push, 3 )
+
+#define WINDOWS_LEAN_AND_MEAN
+#define NOMINMAX
+#define WIN32_LEAN_AND_MEAN
+
+#include <Windows.h>
+
 #endif
 //#if defined(__APPLE__) || defined(__unix__)
 //    #include <GLUT/glut.h>
 //#else
-    #include <GL/glew.h>
-    #include <GL/freeglut.h>
+#include <GL/glew.h>
+#include <GL/freeglut.h>
 //#endif
 #include <cuda_runtime.h>
 #include <cuda_gl_interop.h>
+
 #ifdef _WIN32
 // turn the warnings back on
-    #pragma warning( pop )
+#pragma warning( pop )
 #endif
+
 #include "interactions.h"
 
 // texture and pixel objects
@@ -30,80 +35,84 @@ GLuint tex = 0;     // OpenGL texture object
 struct cudaGraphicsResource *cuda_pbo_resource;
 
 void render() {
-  uchar4 *d_out = 0;
-  cudaGraphicsMapResources(1, &cuda_pbo_resource, 0);
-  cudaGraphicsResourceGetMappedPointer((void **)&d_out, NULL,
-                                       cuda_pbo_resource);
-  kernelLauncher(d_out, W, H, loc);
-  cudaGraphicsUnmapResources(1, &cuda_pbo_resource, 0);
-  char title[256];
-  const char* oldTitle = TITLE_STRING;
-  sprintf(title, "%s: Location = %d, sys = %d",oldTitle, loc.x, loc.y);
-  glutSetWindowTitle(title);
+    uchar4 *d_out = 0;
+    cudaGraphicsMapResources(1, &cuda_pbo_resource, 0);
+    cudaGraphicsResourceGetMappedPointer((void **) &d_out, NULL,
+                                         cuda_pbo_resource);
+    kernelLauncher(d_out, W, H, loc);
+    cudaGraphicsUnmapResources(1, &cuda_pbo_resource, 0);
+    char title[256];
+    const char *oldTitle = TITLE_STRING;
+    sprintf(title, "%s: Location = %d, sys = %d", oldTitle, loc.x, loc.y);
+    glutSetWindowTitle(title);
 }
 
 void drawTexture() {
-  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, W, H, 0, GL_RGBA,
-               GL_UNSIGNED_BYTE, NULL);
-  glEnable(GL_TEXTURE_2D);
-  glBegin(GL_QUADS);
-  glTexCoord2f(0.0f, 0.0f); glVertex2f(0, 0);
-  glTexCoord2f(0.0f, 1.0f); glVertex2f(0, H);
-  glTexCoord2f(1.0f, 1.0f); glVertex2f(W, H);
-  glTexCoord2f(1.0f, 0.0f); glVertex2f(W, 0);
-  glEnd();
-  glDisable(GL_TEXTURE_2D);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, W, H, 0, GL_RGBA,
+                 GL_UNSIGNED_BYTE, NULL);
+    glEnable(GL_TEXTURE_2D);
+    glBegin(GL_QUADS);
+    glTexCoord2f(0.0f, 0.0f);
+    glVertex2f(0, 0);
+    glTexCoord2f(0.0f, 1.0f);
+    glVertex2f(0, H);
+    glTexCoord2f(1.0f, 1.0f);
+    glVertex2f(W, H);
+    glTexCoord2f(1.0f, 0.0f);
+    glVertex2f(W, 0);
+    glEnd();
+    glDisable(GL_TEXTURE_2D);
 }
 
 void display() {
-  render();
-  drawTexture();
-  glutSwapBuffers();
+    render();
+    drawTexture();
+    glutSwapBuffers();
 }
 
 void initGLUT(int *argc, char **argv) {
-  glutInit(argc, argv);
-  glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE);
-  glutInitWindowSize(W, H);
-  glutCreateWindow(TITLE_STRING);
+    glutInit(argc, argv);
+    glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE);
+    glutInitWindowSize(W, H);
+    glutCreateWindow(TITLE_STRING);
 #ifndef __APPLE__
-  glewInit();
+    glewInit();
 #endif
 }
 
 void initPixelBuffer() {
-  glGenBuffers(1, &pbo);
-  glBindBuffer(GL_PIXEL_UNPACK_BUFFER, pbo);
-  glBufferData(GL_PIXEL_UNPACK_BUFFER, 4*W*H*sizeof(GLubyte), 0,
-                GL_STREAM_DRAW);
-  glGenTextures(1, &tex);
-  glBindTexture(GL_TEXTURE_2D, tex);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-  cudaGraphicsGLRegisterBuffer(&cuda_pbo_resource, pbo,
-                               cudaGraphicsMapFlagsWriteDiscard);
+    glGenBuffers(1, &pbo);
+    glBindBuffer(GL_PIXEL_UNPACK_BUFFER, pbo);
+    glBufferData(GL_PIXEL_UNPACK_BUFFER, 4 * W * H * sizeof(GLubyte), 0,
+                 GL_STREAM_DRAW);
+    glGenTextures(1, &tex);
+    glBindTexture(GL_TEXTURE_2D, tex);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    cudaGraphicsGLRegisterBuffer(&cuda_pbo_resource, pbo,
+                                 cudaGraphicsMapFlagsWriteDiscard);
 }
 
 void exitfunc() {
-   if (pbo) {
-    cudaGraphicsUnregisterResource(cuda_pbo_resource);
-    glDeleteBuffers(1, &pbo);
-    glDeleteTextures(1, &tex);
-   }
+    if (pbo) {
+        cudaGraphicsUnregisterResource(cuda_pbo_resource);
+        glDeleteBuffers(1, &pbo);
+        glDeleteTextures(1, &tex);
+    }
 }
 
-int main(int argc, char** argv) {
-  printInstructions();
-  initGLUT(&argc, argv);
-  gluOrtho2D(0, W, H, 0);
-  glutKeyboardFunc(keyboard);
-  glutSpecialFunc(handleSpecialKeypress);
-  glutPassiveMotionFunc(mouseMove);
-  glutMotionFunc(mouseDrag);
-  glutMouseWheelFunc(mouseWheel);
-  glutMouseFunc(mouse);
-  glutDisplayFunc(display);
-  initPixelBuffer();
-  glutMainLoop();
-  atexit(exitfunc);
-  return 0;
+int main(int argc, char **argv) {
+    printInstructions();
+    initGLUT(&argc, argv);
+    gluOrtho2D(0, W, H, 0);
+    glutKeyboardFunc(keyboard);
+    glutSpecialFunc(handleSpecialKeypress);
+    glutPassiveMotionFunc(mouseMove);
+    glutMotionFunc(mouseDrag);
+    glutMouseWheelFunc(mouseWheel);
+    glutMouseFunc(mouse);
+    glutDisplayFunc(display);
+    initPixelBuffer();
+    glutMainLoop();
+    atexit(exitfunc);
+    return 0;
 }
