f46fff41b709ad1834ecb5ffa2bc820bd0005b74
Nik <mail-an-nik0405@web.de>
2021-08-01 14:58:21 +0200

    Added headless start

--- main.cpp
+++ main.cpp
@@ -1,113 +1,45 @@
-#include "kernel.h"
-#include <stdio.h>
-#include <stdlib.h>
-
-#ifdef _WIN32
-    // save diagnostic state
-    #pragma warning( push, 3 )
-    #define WINDOWS_LEAN_AND_MEAN
-    #define NOMINMAX
-    #define WIN32_LEAN_AND_MEAN
-    #include <Windows.h>
-#endif
-#include <GL/glew.h>
-#include <GL/freeglut.h>
+// CUDA standard includes
 #include <cuda_runtime.h>
-#include <cuda_gl_interop.h>
+#include <iostream>
+#include "kernel.h"
 
-#ifdef _WIN32
-    // turn the warnings back on
-    #pragma warning( pop )
-#endif
+cudaEvent_t start, stop;
+inline void startCUDATimer(){
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
 
-#include "interactions.h"
-
-// texture and pixel objects
-GLuint pbo = 0;     // OpenGL pixel buffer object
-GLuint tex = 0;     // OpenGL texture object
-struct cudaGraphicsResource *cuda_pbo_resource;
-
-void render() {
-    uchar4 *d_out = 0;
-    cudaGraphicsMapResources(1, &cuda_pbo_resource, 0);
-    cudaGraphicsResourceGetMappedPointer((void **) &d_out, NULL,
-                                         cuda_pbo_resource);
-    kernelLauncher(d_out, W, H, loc);
-    cudaGraphicsUnmapResources(1, &cuda_pbo_resource, 0);
-
-    // Can be deleted. Shows current location in window title
-    char title[256];
-    const char *oldTitle = TITLE_STRING;
-    sprintf(title, "%s: Location = %d, sys = %d", oldTitle, loc.x, loc.y);
-    glutSetWindowTitle(title);
+    cudaEventRecord( start, 0 );
 }
 
-void drawTexture() {
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, W, H, 0, GL_RGBA,
-                 GL_UNSIGNED_BYTE, NULL);
-    glEnable(GL_TEXTURE_2D);
-    glBegin(GL_QUADS);
-    glTexCoord2f(0.0f, 0.0f);
-    glVertex2f(0, 0);
-    glTexCoord2f(0.0f, 1.0f);
-    glVertex2f(0, H);
-    glTexCoord2f(1.0f, 1.0f);
-    glVertex2f(W, H);
-    glTexCoord2f(1.0f, 0.0f);
-    glVertex2f(W, 0);
-    glEnd();
-    glDisable(GL_TEXTURE_2D);
-}
-
-void display() {
-    render();
-    drawTexture();
-    glutSwapBuffers();
-}
-
-void initGLUT(int *argc, char **argv) {
-    glutInit(argc, argv);
-    glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE);
-    glutInitWindowSize(W, H);
-    glutCreateWindow(TITLE_STRING);
-#ifndef __APPLE__
-    glewInit();
-#endif
-}
-
-void initPixelBuffer() {
-    glGenBuffers(1, &pbo);
-    glBindBuffer(GL_PIXEL_UNPACK_BUFFER, pbo);
-    glBufferData(GL_PIXEL_UNPACK_BUFFER, 4 * W * H * sizeof(GLubyte), 0,
-                 GL_STREAM_DRAW);
-    glGenTextures(1, &tex);
-    glBindTexture(GL_TEXTURE_2D, tex);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-    cudaGraphicsGLRegisterBuffer(&cuda_pbo_resource, pbo,
-                                 cudaGraphicsMapFlagsWriteDiscard);
-}
-
-void exitfunc() {
-    if (pbo) {
-        cudaGraphicsUnregisterResource(cuda_pbo_resource);
-        glDeleteBuffers(1, &pbo);
-        glDeleteTextures(1, &tex);
-    }
+inline float printCUDATime(bool print){
+    cudaEventRecord( stop, 0 );
+    cudaEventSynchronize( stop );
+    float time;
+    cudaEventElapsedTime( &time, start, stop );
+    if(print)
+        std::cout << "Took: "<< time <<"ms" << std::endl;
+    cudaEventDestroy( start );
+    cudaEventDestroy( stop );
+    return time;
 }
 
 int main(int argc, char **argv) {
-    printInstructions();
-    initGLUT(&argc, argv);
-    gluOrtho2D(0, W, H, 0);
-    glutKeyboardFunc(keyboard);
-    glutSpecialFunc(handleSpecialKeypress);
-    glutPassiveMotionFunc(mouseMove);
-    glutMotionFunc(mouseDrag);
-    glutMouseWheelFunc(mouseWheel);
-    glutMouseFunc(mouse);
-    glutDisplayFunc(display);
-    initPixelBuffer();
-    glutMainLoop();
-    atexit(exitfunc);
+    size_t frames = 600;
+    std::cout<< "Simulating "<< frames<<" frames..." <<std::endl;
+    int w = 1600;
+    int h = 1600;
+    uchar4* imageCUDA;
+    gpuErrchk(cudaMalloc((void**)&imageCUDA,w*h*sizeof(imageCUDA)));
+    int2 loc = {0,0};
+    startCUDATimer();
+    for (size_t i = 0; i < 600; ++i) {
+        kernelLauncher(imageCUDA, w, h, loc);
+    }
+    //gpuErrchk( cudaPeekAtLastError() );
+    float time = printCUDATime(false);
+    std::cout << "Took: "<< time <<"ms.\t" << time/frames << "ms/frame.\t "<< 1000/time*frames<<"fps"<< std::endl;
+    //kernel<<<grid,threads>>> ( d_odata, d_idata, size_x, size_y,
+    //        NUM_REPS);
+
     return 0;
 }
